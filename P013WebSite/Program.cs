d797348c386295c4044f839f74012620c9b0a7bc
mesutilica <mesut.ilica@gmail.com>
2023-04-10 23:01:50 +0300

    Add project files.

---
+++ P013WebSite/Program.cs
@@ -0,0 +1,40 @@
+using P013WebSite.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DatabaseContext>(); // burada veritabaný iþlemleri için kullandýðýmýz contexti tanýtýyoruz
+// veritabaný oluþturmak için üst menüden Tools > Nuget Package Manager > Package manger console menüsünden komut yazma ekranýný açýyoruz.
+// Bu ekrana add-migration InitialCreate yazýp enter a basýyoruz.
+// Migrations klasörü ve initialcreate classý oluþtuktan sonra update-database yazýp enter a basýyoruz
+// Done mesajý geldiyse iþlem baþarýlýdýr.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+            name: "admin",
+            pattern: "{area:exists}/{controller=Main}/{action=Index}/{id?}"
+          );
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
