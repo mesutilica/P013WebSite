97c45a9ae83b29f8164cb4af9e683b4dfcca705c
mesutilica <mesut.ilica@gmail.com>
2023-04-25 22:35:29 +0300

    25-04-2023 v2

---
+++ P013WebSite/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P013WebSite.Data;
+
+namespace P013WebSite.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public CategoriesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(int? id)
+        {
+            if (id is null) // eğer adres çubuğundan id gönderilmemişse
+            {
+                return BadRequest(); // ekrana geçersiz istek hatası ver
+            }
+            var category = _context.Categories.Include(p => p.Products).FirstOrDefault(c => c.Id == id); // id gönderilmişse db den o kategoriyi ara
+            if (category == null) // eğer kategori db de yoksa
+            {
+                return NotFound(); // geriye bulunamadı hatası döndür
+            }
+            return View(category); // kategori bulunduysa ekrana yolla
+        }
+    }
+}
---
+++ P013WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P013WebSite.Data;
+
+namespace P013WebSite.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public ProductsController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> DetailAsync(int? id)
+        {
+            if (id is null)
+            {
+                return BadRequest();
+            }
+            var product = await _context.Products.Include("Category").FirstOrDefaultAsync(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+    }
+}
