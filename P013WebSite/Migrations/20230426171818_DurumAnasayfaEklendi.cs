0890abf757309ec934a10a9a367aec2a71a46f65
mesutilica <mesut.ilica@gmail.com>
2023-04-26 20:53:11 +0300

    26-04-2023 view model eklendi

---
+++ P013WebSite/Migrations/20230426171818_DurumAnasayfaEklendi.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace P013WebSite.Migrations
+{
+    /// <inheritdoc />
+    public partial class DurumAnasayfaEklendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHome",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2023, 4, 26, 20, 18, 18, 157, DateTimeKind.Local).AddTicks(3562));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreateDate",
+                value: new DateTime(2023, 4, 26, 20, 18, 18, 157, DateTimeKind.Local).AddTicks(3589));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2023, 4, 26, 20, 18, 18, 157, DateTimeKind.Local).AddTicks(2913));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "IsHome",
+                table: "Products");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2023, 4, 10, 22, 5, 32, 880, DateTimeKind.Local).AddTicks(340));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreateDate",
+                value: new DateTime(2023, 4, 10, 22, 5, 32, 880, DateTimeKind.Local).AddTicks(342));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreateDate",
+                value: new DateTime(2023, 4, 10, 22, 5, 32, 880, DateTimeKind.Local).AddTicks(150));
+        }
+    }
+}
